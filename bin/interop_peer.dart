280bfd243b4fd5a216238c98d5803f892f09454d
Brian Takita <brian.takita@gmail.com>
2026-09-15 08:53:31 -0400

    fix: align interop version with v0.31.0

--- bin/interop_peer.dart
+++ bin/interop_peer.dart
@@ -66,7 +66,7 @@ class _InteropPeer {
     return {
       'ok': true,
       'binding': 'lazily-dart',
-      'version': '0.29.0',
+      'version': '0.31.0',
       'protocol_version': _protocolVersion,
       'features': [
         'distributed_crdt',
